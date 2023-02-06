c067f4e1f8843b36c611970a969137949d3176e5
shigaboba <shigapovalrm@gmail.com>
2023-02-06 23:13:16 +0300

    Homework is done

--- src/Homework12/Author.java
+++ src/Homework12/Author.java
@@ -1,5 +1,22 @@
 package Homework12;
 
 public class Author {
-    String FirstName;
+    String firstName;
+    String lastName;
+
+    public Author(String firstName, String lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
 }
+
+
